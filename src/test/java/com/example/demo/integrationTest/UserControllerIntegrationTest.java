6537cc03d460ae852026461685b6800ea715f12b
Manoj M <manumadhu1425@gmail.com>
2023-06-25 19:50:43 +0530

    Integration test using hamcrest (not worked)

---
+++ src/test/java/com/example/demo/integrationTest/UserControllerIntegrationTest.java
@@ -0,0 +1,32 @@
+package com.example.demo.integrationTest;
+
+import com.example.demo.entity.User;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class UserControllerIntegrationTest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    public void getAllUsers() throws Exception{
+        ResponseEntity<List> response = this.testRestTemplate.getForEntity("http://localhost:"+this.port+"/api/users/",List.class);
+        MatcherAssert.assertThat(response.getStatusCode(), CoreMatchers.equalTo(HttpStatus.OK));
+    }
+
+}
