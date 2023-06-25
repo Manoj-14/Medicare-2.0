d2d850bf924b289a75772ee36a2fe98b9da43cda
Manoj M <manumadhu1425@gmail.com>
2023-06-25 08:43:28 +0530

    Password change operation updated

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
 import com.example.demo.service.UserServiceImpl;
+import com.example.demo.utils.Log;
 import jakarta.persistence.EntityNotFoundException;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,10 +47,12 @@ public class UserController {
     }
 
     @PutMapping("/changePassword/{id}")
-    public ResponseEntity<?> changePassword(@PathVariable int id,@ModelAttribute String old_password, @ModelAttribute String new_password){
+    public ResponseEntity<?> changePassword(@PathVariable int id,@RequestParam String old_password, @RequestParam String new_password){
         try{
+            Log.INFO("Old Password:"+old_password);
+            Log.INFO("New Password:"+new_password);
             userService.changePassword(id,old_password,new_password);
-            return ResponseEntity.accepted().build();
+            return new ResponseEntity<>("Password changed", HttpStatus.OK);
         }catch (EntityNotFoundException ene){
             throw new ResponseStatusException(HttpStatus.NOT_FOUND,"User not found");
         }catch (VerifyError ve){
