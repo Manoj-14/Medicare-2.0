3c623a2de9d8c6eb0c744eb112db464a9bd2486a
Manoj M <manumadhu1425@gmail.com>
2023-06-26 11:06:10 +0530

    Admin Controller added

---
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -0,0 +1,55 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Admin;
+import com.example.demo.service.AdminService;
+import com.example.demo.utils.Log;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/admin/")
+public class AdminController {
+    @Autowired
+    AdminService adminService;
+
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody Admin admin){
+        try{
+            int id = adminService.create(admin);
+            return new ResponseEntity<>("Admin "+admin.getName()+" created successfully",HttpStatus.CREATED);
+        }catch(EntityExistsException ee){
+            throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE,"Admin already exists");
+        }
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<?> getAdmin(@PathVariable String email){
+        try{
+            Admin admin = adminService.find(email);
+            return new ResponseEntity<>(admin, HttpStatus.OK);
+        }catch (EntityNotFoundException enf){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Admin not found");
+        }
+    }
+
+    @PostMapping("/authenticate/{email}")
+    public ResponseEntity<?> authenticate(@PathVariable String email, @RequestBody Map<String,String> request){
+        String password = request.get("password");
+        try{
+            Log.DEBUG(email+":"+password);
+            Admin admin = adminService.authenticate(email,password);
+            return new ResponseEntity<>(admin, HttpStatus.OK);
+        }catch (EntityNotFoundException enf){
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,"Admin credentials not found");
+        }
+    }
+
+
+}
