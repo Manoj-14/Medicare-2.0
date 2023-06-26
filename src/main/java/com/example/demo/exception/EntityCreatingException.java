4367411f69c8e80662982af2090e006b9234f771
Manoj M <manumadhu1425@gmail.com>
2023-06-26 17:40:02 +0530

    EntityCreatingException Added

---
+++ src/main/java/com/example/demo/exception/EntityCreatingException.java
@@ -0,0 +1,9 @@
+package com.example.demo.exception;
+
+public class EntityCreatingException extends Exception{
+
+    public EntityCreatingException(){}
+    public EntityCreatingException(String message){
+        super(message);
+    }
+}
